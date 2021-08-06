ff9c9917af8a28a4f0c52d0ba29945bdb36158ea
vidhen <vidhuchauhan2912@gmail.com>
2021-08-06 10:08:22 +0500

    day12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+  Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alzashop.com%2Fiphone-12-pro-128gb-seda-d6216469.htm&psig=AOvVaw3-TrgZl4cr5_IUsbtMJf2f&ust=1628311085056000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjf9ffJm_ICFQAAAAAdAAAAABAK"),
+];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,14 +21,3 @@ class Item {
 
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
-
-final products = [
-  Item(
-      id: "Codepur001",
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freeiconspng.com%2Fimg%2F7359&psig=AOvVaw0J9VomFNzWbDjaTSGwfFSL&ust=1628168886205000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPCElIi4l_ICFQAAAAAdAAAAABAD"),
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:first_pro/models/catalog.dart';
 import 'package:first_pro/widgets/drawer.dart';
+import 'package:first_pro/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,13 +8,20 @@ class HomePage extends StatelessWidget {
   final String name = "Codepur";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("Catalog App")),
       ),
-      body: Center(
-        child: Container(
-          child: Text("welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
